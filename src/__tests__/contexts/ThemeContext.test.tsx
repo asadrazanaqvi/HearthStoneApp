f1b56d7d72b9509f3ea9d335ad5a9feedf08d4c0
asadrazanaqvi <asad.naqvi89@gmail.com>
2025-05-27 15:04:05 +0530

    library error resolved

--- src/__tests__/contexts/ThemeContext.test.tsx
+++ src/__tests__/contexts/ThemeContext.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { renderHook, act } from '@testing-library/react-hooks';
+import {renderHook, act} from '@testing-library/react-native'
 import { ThemeProvider, useTheme } from '../../contexts/ThemeContext';
 
 describe('ThemeContext', () => {
